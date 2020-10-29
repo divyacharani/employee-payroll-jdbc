686b2e1cb527edec7fa269b0702b4564b2066374
unknown <divyacharaninabha@gmail.com>
2020-10-29 11:16:08 +0530

    Added: Welcome message added

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EmployeePayrollService {
+
+	private static final Logger LOG = LogManager.getLogger(EmployeePayrollService.class);
+
+	public static void main(String[] args) {
+		// Welcome Message
+		LOG.info("Welcome to Employee Payroll Service");
+
+	}
+
+}
